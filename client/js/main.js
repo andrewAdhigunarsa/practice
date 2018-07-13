4d6ea3058271fc9d7e0d182c869da0395bd538af
Andrew Adhigunarsa <andrew.adhigunarsa@gmail.com>
2018-07-13 09:13:45 +0000

    adding new files into practice repo

---
+++ client/js/main.js
@@ -0,0 +1,49 @@
+let form = document.querySelector(".search-form")
+let resultShow = document.querySelector(".result-show")
+let button = document.querySelector(".search")
+let input = document.querySelector(".searchinput")
+let audio = document.querySelector("audio")
+
+
+// text value need to storage when the submit button action happen
+form.addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    let searchText = document.querySelector("#searchinput").value
+
+    console.log("searchText", searchText)
+
+    resultShow.textContent = ""
+
+    fetch(`https://itunes.apple.com/search?term=${searchText}`)  
+    .then (function (data){
+        return data.json();
+    })
+    .then (function(json) {
+        console.log(json)   
+    
+        for (var i = 0; i < json.results.length; i++) {
+            let name = json.results[i].artistName
+            let img = json.results[i].artworkUrl100
+            let songName = json.results[i].collectionName
+            let audio2 = json.results[i].previewUrl
+    
+            let show = `
+             <div class="col-md-3">
+                  <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="${img}" value= "${audio2}" alt="Card image ">
+                    <div class="card-body">
+                        <h3> ${name} </h3>
+                        <h4> ${songName}</h4>
+                        <audio controls class="audiocontrol">
+                        <source value="" src="${audio2}" type="audio/mpeg"> 
+                    </div>
+                  </div>
+            </div>
+            `
+
+            resultShow.insertAdjacentHTML("beforeEnd", show)
+        }
+    })
+})
+
